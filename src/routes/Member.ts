45ddc827f502d987cb38b8ee6759cfca2e59346f
ScriptedPranav <sinhafighterpranav@gmail.com>
2023-06-18 22:06:00 +0530

    create delete/member/id endpoint

--- src/routes/Member.ts
+++ src/routes/Member.ts
@@ -157,7 +157,7 @@ router.post("/", async (req: Request, res: Response) => {
   }
 });
 
-function getUserIdFromAccessToken(accessToken: string): string | null {
+function getUserIdFromAccessToken(accessToken: string) {
   try {
     const decodedToken: any = jwt.verify(
       accessToken,
@@ -188,4 +188,90 @@ async function checkIfUserIsCommunityAdmin(
 
   return false;
 }
+
+router.delete("/:id", async (req: Request, res: Response) => {
+    try {
+      // Get the member ID from the request parameters
+      const memberId = req.params.id;
+  
+      // Check if the member exists in the database
+      const existingMember = await db.member.findUnique({
+        where: { id: memberId },
+      });
+  
+      if (!existingMember) {
+        return res.status(404).json({
+          status: false,
+          errors: [
+            { message: "Member not found.", code: "RESOURCE_NOT_FOUND" },
+          ],
+        });
+      }
+  
+      // Get the access token from the authorization header
+      const accessToken = req.headers.authorization?.split(" ")[1] || "";
+  
+      // Check if the user is a community admin or moderator
+      const isAdminOrModerator = checkIfUserIsCommunityAdminOrModerator(accessToken);
+  
+      if (!isAdminOrModerator) {
+        return res.status(403).json({
+          status: false,
+          errors: [
+            {
+              message: "You are not authorized to perform this action.",
+              code: "NOT_ALLOWED_ACCESS",
+            },
+          ],
+        });
+      }
+  
+      // Remove the member from the database
+      await db.member.delete({ where: { id: memberId } });
+  
+      // Return the success response
+      return res.status(200).json({
+        status: true,
+      });
+    } catch (error) {
+      // Return a generic error response for any errors
+      return res.status(500).json({
+        status: false,
+        errors: [
+          { message: "Internal Server Error", code: "INTERNAL_SERVER_ERROR" },
+        ],
+      });
+    }
+  });
+
+  async function checkIfUserIsCommunityAdminOrModerator(accessToken: string) {
+    // Get the user ID from the access token
+    const userId = getUserIdFromAccessToken(accessToken);
+  
+    // Retrieve the user's roles from the database
+    const user = await db.user.findUnique({
+        where: { id: userId },
+        include: {
+          member: {
+            include: {
+              community_fk: {
+                select: { id: true },
+              },
+              role_fk: {
+                select: { name: true },
+              },
+            },
+          },
+        },
+      });
+  
+    // Check if the user has the "community_admin" or "community_moderator" role
+    const isAdminOrModerator = user?.member.some(
+      (member) =>
+        member.role_fk.name === "Community Admin" || member.role_fk.name === "Community Moderator"
+    );
+  
+    return isAdminOrModerator;
+  }
+  
 export default router;
